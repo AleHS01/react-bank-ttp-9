af4beac30e2b10109fecb38f87e1837b133ade94
Alejandro Hernandez <alejandrohernandezsantos01@gmail.com>
2023-06-20 23:03:00 -0400

    balance initial display fix

--- src/App.js
+++ src/App.js
@@ -9,30 +9,44 @@ import Debits from "./components/Debits";
 
 const App = () => {
   const [balance, setBalance] = useState(0);
-  const [debit, setDebit] = useState(Number(200).toFixed(2));
-  const [credit, setCredit] = useState(Number(100).toFixed(2));
+  // Both are undefined so the Balance load only when both have a value, to avoid the user see a 0 in balance first
+  const [debit, setDebit] = useState(undefined);
+  const [credit, setCredit] = useState(undefined);
 
   useEffect(() => {
-    // const getDebit = async () => {
-    //   try {
-    //     const response = await axios.get(
-    //       "https://bank-of-react-b745wfs0u-ajlapid718.vercel.app/debits"
-    //     );
-    //     console.log(response);
-    //   } catch (error) {
-    //     console.log(error);
-    //   }
-    // };
-    //setBalance(Number(credit - debit).toFixed(2));
-  }, [balance, debit, credit]);
+    const getData = async () => {
+      try {
+        const creditResponse = await axios.get(
+          "https://bank-of-react-b745wfs0u-ajlapid718.vercel.app/credits"
+        );
+        const debitResponse = await axios.get(
+          "https://bank-of-react-b745wfs0u-ajlapid718.vercel.app/debits"
+        );
+        setCredit(creditResponse.data);
+        setDebit(debitResponse.data);
+        // I don't use debit & credit dirrectly cause they will load value after the function is done
+        setBalance(creditResponse.data - debitResponse.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getData();
+  }, []);
 
   return (
     <Router>
       <div className="App">
         <header>
-          <p>Balance: ${balance}</p>
-          <p>Debit: ${debit}</p>
-          <p>Credit: ${credit}</p>
+          {/* if credit and debit are defined then lets render the data, else let display loading screen */}
+          {credit !== undefined && debit !== undefined ? (
+            <div>
+              <p>Balance: ${balance.toFixed(2)}</p>
+              <p>Debit: ${debit.toFixed(2)}</p>
+              <p>Credit: ${credit.toFixed(2)}</p>
+            </div>
+          ) : (
+            <div className="loading-div">Loading...</div>
+          )}
         </header>
         {/* Navigation */}
         <nav>
